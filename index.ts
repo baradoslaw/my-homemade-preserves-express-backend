085fef1c9cb837eeb7ba152b0c03e9c0c7e7deb9
RadoslawBaran <baradoslaw@gmail.com>
2023-03-27 16:08:43 +0200

    Init My Homemade Preserves backend. Uses express.js, TypeScript and mysql2.

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+app.use(express.json());
+
+app.listen(3001, '0.0.0.0', () => {
+  console.log('App listening on http://localhost:3001');
+});
