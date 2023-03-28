ce304a3bb9285ef80395a91068830d25876e7767
RadoslawBaran <baradoslaw@gmail.com>
2023-03-28 09:36:54 +0200

    Add tests for checking hashes.

--- tests/bcrypt-hash.test.ts
+++ tests/bcrypt-hash.test.ts
@@ -1,8 +1,27 @@
-import {makeHash} from "../utils/hash";
+import {checkHash, makeHash} from "../utils/hash";
 
 test('makeHash creates valid bcrypt hash (60 characters).', async () => {
   const text = 'Example password';
   const newHash = await makeHash(text);
 
   expect(newHash.length).toBe(60);
+});
+
+test('checkHash returns true when provided with string and bcrypt hash based on it.', async () => {
+  const text = 'Example password';
+  const hash = await makeHash(text);
+
+  const isMatched = await checkHash(text, hash);
+
+  expect(isMatched).toBe(true);
+});
+
+test('checkHash returns false when provided with string and bcrypt hash based on other text.', async () => {
+  const text1 = 'Example password 1';
+  const text2 = 'Example password 2';
+  const hash = await makeHash(text1)
+
+  const isMatched = await checkHash(text2, hash);
+
+  expect(isMatched).toBe(false);
 });
