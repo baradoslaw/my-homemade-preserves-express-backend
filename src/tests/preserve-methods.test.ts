f1c35d84e8f83ea1ce7b2e1fc68c26d813fe4cce
RadoslawBaran <baradoslaw@gmail.com>
2023-04-06 12:29:01 +0200

    Add tests for PreserveRecord.insert method.

--- src/tests/preserve-methods.test.ts
+++ src/tests/preserve-methods.test.ts
@@ -2,13 +2,14 @@ import {pool} from "../../utils/db";
 import {PreserveRecord} from "../../records/preserve.record";
 
 const defaultPreserve = {
-  name: 'Ogórki kiszone',
+  name: 'Test #1 ogórki kiszone',
   description: 'Tradycyjne polskie ogórki kiszone',
   typeName: 'Kiszonka',
   userId: 'dec63404-66ad-43c1-b483-47701306ee91', // Test1
 }
 
 afterAll(async () => {
+  await pool.execute("DELETE FROM `preserve` WHERE `name` = 'Test #1 ogórki kiszone'");
   await pool.end();
 });
 
@@ -42,6 +43,35 @@ test('PreserveRecord.getAllPreservesForUser returns empty array, when there is n
   expect(preserves.length).toBe(0);
 });
 
-// test('PreserveRecord.insert inserts data to database.', async () => {
-//   const preserve = new
-// });
+test('PreserveRecord.insert inserts data to database.', async () => {
+  const preserve = new PreserveRecord(defaultPreserve);
+  await preserve.insert();
+
+  const foundPreserve = await PreserveRecord.getOne(preserve.id);
+
+  expect(foundPreserve).toBeDefined();
+  expect(foundPreserve).not.toBeNull();
+  expect(foundPreserve.id).toBe(preserve.id);
+});
+
+test('PreserveRecord.insert does not allow to save entry with user id that does not exist.', async () => {
+  const preserve = new PreserveRecord({
+    ...defaultPreserve,
+    userId: 'user does not exist',
+  });
+
+  await expect(async () => {
+    await preserve.insert();
+  }).rejects.toThrowError('Dany użytkownik nie istnieje.');
+});
+
+test('PreserveRecord.insert does not allow to save antry with id that is already in database.', async () => {
+  const preserve = new PreserveRecord({
+    ...defaultPreserve,
+    id: '843cf290-8bbc-43e9-90e6-6b0a50d529e5',
+  });
+
+  await expect(async () => {
+    await preserve.insert();
+  }).rejects.toThrowError('Przetwór o podanym id już istnieje.');
+});
