dc3b2278c3e23536cb581dd0b429b77c5b324bfc
RadoslawBaran <baradoslaw@gmail.com>
2023-04-07 15:44:36 +0200

    Add endpoint in userRouter for logging in.

--- routers/user.router.ts
+++ routers/user.router.ts
@@ -1,5 +1,7 @@
 import {Router} from "express";
 import {UserRecord} from "../records/user.record";
+import {checkHash} from "../utils/hash";
+import {ValidationError} from "../utils/errors";
 
 export const userRouter = Router()
   .post('/new-user', async (req, res) => {
@@ -9,4 +11,13 @@ export const userRouter = Router()
     await user.insert();
 
     res.json(user.id);
+  })
+
+  .get('/log-in', async (req, res) => {
+      const user = await UserRecord.getOneByLogin(req.body.login);
+      if (await checkHash(req.body.pwd, user.pwd)) {
+          res.json(user);
+      } else {
+          throw new ValidationError('Niepoprawny login lub hasło.');
+      }
   });
